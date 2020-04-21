3b46219b45eeca809c4c112fe3d86293ae0cb69c
Andrea-Giugie <bisateo@gmail.com>
2020-04-21 02:08:00 +0200

    top menu

--- Valorant/src/app/app.module.ts
+++ Valorant/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
