79f17d347b6f13e0496fc1aca4052de53a6a7e1e
Andrea-Giugie <bisateo@gmail.com>
2020-04-26 17:52:16 +0200

    Landing Page

--- Valorant/src/app/app.module.ts
+++ Valorant/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
+import { RicercaComponent } from './ricerca/ricerca.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    RicercaComponent
   ],
   imports: [
     BrowserModule,
---
+++ Valorant/src/app/ricerca/ricerca.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Ricerca',
+  templateUrl: './ricerca.component.html',
+  styleUrls: ['./ricerca.component.scss']
+})
+export class RicercaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
